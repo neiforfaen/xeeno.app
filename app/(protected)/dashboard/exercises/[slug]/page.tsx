495cd7a3e41e6b339ab28401898178692a1bf44e
neiforfaen <180103988+neiforfaen@users.noreply.github.com>
2026-07-13 17:55:44 +0200

    refactor: return not found on non-valid exercise paths

--- app/(protected)/dashboard/exercises/[slug]/page.tsx
+++ app/(protected)/dashboard/exercises/[slug]/page.tsx
@@ -1,9 +1,22 @@
+import { notFound } from "next/navigation"
+
+const validatePath = (value: string) => {
+  switch (value) {
+    case "react":
+      return
+
+    default:
+      return notFound()
+  }
+}
+
 export default async function ExercisePage({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
+  validatePath(slug)
 
   return <div>{slug}</div>
 }
